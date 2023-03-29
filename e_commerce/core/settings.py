6e80bb2a9e94568d8ad8b3ced8a86f495850eeb4
Akshay Kannan <Akshay.kannan@epixelsolutions.com>
2023-03-29 15:43:03 +0530

    docker1

--- e_commerce/core/settings.py
+++ e_commerce/core/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'), '0.0.0.0']
 
 # Application definition
 MEDIA_URL = "/media/"
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'django1', 
         'USER': 'postgres', 
         'PASSWORD': 'psql',
-        'HOST': '127.0.0.1', 
+        'HOST': 'postgres_db', 
         'PORT': '5432',
     }
 }
